f4bc5f3ae4f86010e05e95e7dc3c86e8bc13165a
ayushisingh1209 <ayushisingh12092003@gmail.com>
2024-04-04 00:20:19 +0530

    deploy

---
+++ js/jav.js
@@ -0,0 +1,60 @@
+function showff(){
+    var div1= document.getElementById("fastfood");
+    if(div1.style.display!="none"){
+        div1.style.display="none";
+    }
+    else{
+        div1.style.display="block";
+    }
+};
+function showch(){
+    var div= document.getElementById("chinese");
+    if(div.style.display!="none"){
+        div.style.display="none";
+    }
+    else{
+        div.style.display="block";
+    }
+};
+function showit(){
+    var div= document.getElementById("italian");
+    if(div.style.display!="none"){
+        div.style.display="none";
+    }
+    else{
+        div.style.display="block";
+    }
+    
+};
+
+function showsi(){
+    var div= document.getElementById("southindian");
+    if(div.style.display!="none"){
+        div.style.display="none";
+    }
+    else{
+        div.style.display="block";
+    }
+};
+function showd(){
+    var div= document.getElementById("drinks");
+    if(div.style.display!="none"){
+        div.style.display="none";
+    }
+    else{
+        div.style.display="block";
+    }
+};
+function showi(){
+    var div= document.getElementById("indian");
+    if(div.style.display!="none"){
+        div.style.display="none";
+    }
+    else{
+        div.style.display="block";
+    }
+};
+
+function myfun(){
+    window.alert('Thanks for ordering this :-)');
+}
